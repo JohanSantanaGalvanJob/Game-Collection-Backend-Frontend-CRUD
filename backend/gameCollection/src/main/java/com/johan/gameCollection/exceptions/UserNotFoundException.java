97f26f6330d27badaa4a741c7995559e7cbcdad3
Johan <johansantanagalvan.job@gmail.com>
2022-10-02 21:04:36 +0100

    finished

--- backend/gameCollection/src/main/java/com/johan/gameCollection/exceptions/UserNotFoundException.java
+++ backend/gameCollection/src/main/java/com/johan/gameCollection/exceptions/UserNotFoundException.java
@@ -6,4 +6,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "User Not found")
 public class UserNotFoundException  extends RuntimeException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 }
